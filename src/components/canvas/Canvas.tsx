db9e26265412946db51cceec2c3dc32a3e6c20bf
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-06-23 23:52:30 +0200

    remove overflow-y: scroll for auto

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -68,7 +68,7 @@ export const Canvas: React.FC<CanvasProps> = ({
   // Dynamically set canvas height based on entry positions
   useEffect(() => {
     if (!canvasSize.height) return;
-    const buffer = 200;
+    const buffer = 100;
     let maxY = 0;
     entries.forEach((entry) => {
       const bottom = entry.position.yCord + 150; // approx. entry height
